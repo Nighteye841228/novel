e09db8da7a6b781b744ee2aa8d707cb3487186e2
Shion <a951753abc@gmail.com>
2018-03-14 00:00:39 +0800

    migrations add example

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'root@gmail.com',
+            'type' => config('const.admin'),
+            'password' => password_hash('root1234', PASSWORD_DEFAULT),
+        ]);
     }
 }
