9bb2a57e815cf87416154bf407f8ee38d7612f17
Shion <a951753abc@gmail.com>
2018-03-15 23:57:25 +0800

    fix novels table migrate

--- database/migrations/2018_03_15_093825_create_novels_table.php
+++ database/migrations/2018_03_15_093825_create_novels_table.php
@@ -16,8 +16,8 @@ class CreateNovelsTable extends Migration
         Schema::create('novels', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->timestamp('apply_start')->comment('報名開始時間');
-            $table->timestamp('apply_end')->comment('報名結束時間');
+            $table->timestamp('apply_start')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('報名開始時間');
+            $table->timestamp('apply_end')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('報名結束時間');
             $table->enum('type', ['A', 'B'])->comment('規則類型');
             $table->json('num_content')->comment('定義每一家數目，且有開始、結束上傳時間，及可否遲交');
             $table->timestamps();
